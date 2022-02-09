430f541d401b05d1efc2cb4e0f7c5595b4efeb80
Matt Farrell <matthew.farrell2@marist.edu>
2022-02-09 18:10:09 -0500

    Matched open bracket in text file
    Successfully recognized { in text file and outputted to command line

--- src/ClassyCompilerMain.java
+++ src/ClassyCompilerMain.java
@@ -1,5 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ClassyCompilerMain { //good name? maybe, maybe not...but maybe?
-    public static void main(String[] args){
+
+    public static void matchTokens(String inputFile) throws IOException{
+        //Read Sample/Input File
+        File sampleProgram = new File(inputFile);
+		BufferedReader lineScanner = new BufferedReader(new FileReader(sampleProgram));
+
+        //Variable Declaration
+		Matcher lexemeValue;
+        String currentLine;
+
+        //REGEX pattern matching for creating tokens and matching lexemes
+        Pattern openBracketToken = Pattern.compile("\\{");
+        Pattern closeBracketToken = Pattern.compile("\\}");
+
+		while ((currentLine = lineScanner.readLine()) != null) { //while current line in text file is not blank
+            
+            lexemeValue = openBracketToken.matcher(currentLine);
+            if (lexemeValue.find()) {
+                System.out.println("DEBUG Lexer - " + ClassyCompilerTokenTypes.LEFT_BRACKET + " [ " + currentLine.substring(lexemeValue.start(), lexemeValue.end()) + " ] found at line: " );//+ lineNumber);
+                currentLine = currentLine.substring(lexemeValue.end());
+                currentLine = currentLine.trim();
+                lexemeValue = openBracketToken.matcher(currentLine);
+					continue;
+                }
+            }
+    }
+    public static void main(String[] args) throws IOException{
+        System.out.println("\n------------- Welcome To The Classy Compiler! -------------\n");
+        
+        matchTokens(args[0]);
+        //call match tokens, args[0] represents the sample program file
+        //when running java ClassyCompilerMain sampleProgram.txt
+
+
 
     }
     
--- src/ClassyCompilerTokenTypes.java
+++ src/ClassyCompilerTokenTypes.java
@@ -1,15 +1,17 @@
-public class ClassyCompilerTokenTypes { //Fun name? Maybe, maybe not...but maybe!
-	
-	public enum TokenType {
+/* I originally had this named as a class with enum inside,
+	but it doesn't allow me to import the enum, removing the class name
+	fixed the issue.
+*/
+	public enum ClassyCompilerTokenTypes {
 	  
 		LEFT_BRACKET("T_OPEN_BRACE"),
 		RIGHT_BRACKET("T_CLOSE_BRACE"),
 		EOP("T_EOP");
 	
 	
-		private TokenType(String nameOfToken) { //this allows me to have the corresponding string values
+		private ClassyCompilerTokenTypes(String nameOfToken) { //this allows me to have the corresponding string values
 												//for the token names
 		}
 	}
-}
+
 	
