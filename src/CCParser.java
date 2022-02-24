cd9e56480acf135631204dbe0f4e47f750c75316
Matt Farrell <matthew.farrell2@marist.edu>
2022-02-23 19:06:17 -0500

    comments
    comments for logic/my thought process

--- src/CCParser.java
+++ src/CCParser.java
@@ -7,16 +7,21 @@ public class CCParser {
     public boolean result;
     static int indexOfToken = 0;
 
+    //Overall result of parse
     public boolean parseOutcome(ArrayList<CCToken> tokenStream) {
 
         resultOfParse = program();
         return resultOfParse;
     }
 
+    //Checking if input is a valid program
     public boolean program() {
+        //does the input start with [ { ], if yes
+            //then program is valid so far
         if (blockStart() == true) {
+            //is the next token stream [ } ] ?
             if (blockEnd() == true){
-                result = true;
+                result = true; //yay valid program!
             }
         }
         else {
